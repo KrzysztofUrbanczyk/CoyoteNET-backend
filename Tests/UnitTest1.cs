9f6307277f49a9f4c65b5b5b6768b5d068a552d1
dotNETSanta <45518533+dotNETSanta@users.noreply.github.com>
2019-03-25 23:00:25 +0100

    InMemoryDatabase for tests

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -1,14 +1,25 @@
+using DAL;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 using System;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace Tests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        private readonly Context db;
+        public UnitTest1()
         {
-            Assert.Equal(1, 1);
+            var optionsBuilder = new DbContextOptionsBuilder<Context>();
+            optionsBuilder.UseInMemoryDatabase("database");
+            db = new Context(optionsBuilder.Options);
+        }
+        [Fact]
+        public async Task Test1()
+        {
+            Assert.Empty(await db.Users.ToListAsync());
         }
     }
 }
