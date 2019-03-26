a9d1fcffaa23be4dc4a613506303d56ee237090b
Jakub Kowalski <jakub98kowalski@gmail.com>
2019-03-26 22:49:51 +0100

    Project structure proposition.

---
+++ backend/Coyote/CoyoteNETCore.Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using CoyoteNETCore.DAL;
+using CoyoteNETCore.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace CoyoteNETCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : DefaultController
+    {
+        private readonly TestService _service;
+        private readonly Context _db;
+
+        public HomeController(Context db, TestService service)
+        {
+            _db = db;
+            _service = service;
+        }
+
+        [HttpPost("confirm")]
+        [ProducesResponseType((int)HttpStatusCode.OK)]
+        public IActionResult SampleEndpoint()
+        {
+            _service.Test();
+            return Ok();
+        }
+    }
+}
---
+++ backend/Coyote/CoyoteNETCore.Runner/Startup.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using CoyoteNETCore.Controllers;
+using CoyoteNETCore.Services;
+using CoyoteNETCore.DAL;
+
+namespace Coyote.NETCore
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<Context>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("Default")),
+                ServiceLifetime.Transient);
+
+            services.AddTransient<TestService>();
+
+            services
+                .AddMvc()
+                .AddApplicationPart(typeof(HomeController).Assembly)
+                .AddControllersAsServices()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, Context context)
+        {
+            // in order to setup this project easier
+            context.Database.EnsureCreated();
+
+            app.UseForwardedHeaders(new ForwardedHeadersOptions
+            {
+                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+            });
+
+            app.UseAuthentication();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            //app.UseHttpsRedirection();
+
+            app.UseMvc();
+        }
+    }
+}
